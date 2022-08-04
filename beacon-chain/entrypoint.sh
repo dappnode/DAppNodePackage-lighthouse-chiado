dcbba3e833205885e7cc0e7552c55913786c9747
Tropicar <zerozept@protonmail.com>
2022-08-04 15:22:32 +0200

    Beacon configuration
    We have to add the flag for the config file and the flag for the bootnodes, the rest is done

---
+++ beacon-chain/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Concatenate EXTRA_OPTS string
+[[ -n "$CHECKPOINT_SYNC_URL" ]] && EXTRA_OPTS="${EXTRA_OPTS} --checkpoint-sync-url=${CHECKPOINT_SYNC_URL}"
+
+##Required to add the bootnode flag and config file flag
+
+exec lighthouse \
+    --debug-level $DEBUG_LEVEL \
+    beacon_node \
+    --datadir /root/.lighthouse \
+    --http \
+    --http-allow-origin "*" \
+    --http-address 0.0.0.0 \
+    --http-port $BEACON_API_PORT \
+    --port $P2P_PORT \
+    --metrics \
+    --metrics-address 0.0.0.0 \
+    --metrics-port 8008 \
+    --metrics-allow-origin "*" \
+    --execution-endpoint $HTTP_ENGINE \
+    --execution-jwt "/jwtsecret" \
+    $EXTRA_OPTS
